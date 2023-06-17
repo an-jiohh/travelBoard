02454d991e7ce85d59989defe3f64c9e0f87570e
an-jiohh <dkswlgh99@naver.com>
2023-06-17 16:15:20 +0900

    Initial commit

---
+++ src/test/java/com/example/travelboard/TravelBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
